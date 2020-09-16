77b20b482ea0c856c2319f843a53fe36c888e442
Jash <jashjshah13@gmail.com>
2020-09-16 22:33:50 +0530

    Quiz_App:implemented oops concept

---
+++ app2/lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.blue[100],
+      appBar: AppBar(
+        title: Text("App bar"),
+        backgroundColor: Colors.blue[100],
+      ),
+      body: MAinPage(),
+    ),
+  ));
+}
+
+class MAinPage extends StatefulWidget {
+  @override
+  _MAinPageState createState() => _MAinPageState();
+}
+
+class _MAinPageState extends State<MAinPage> {
+  int leftEmoji = 1, rigthEmoji = 6;
+  void changeDice() {
+    setState(() {
+      leftEmoji = Random().nextInt(6) + 1;
+      rigthEmoji = Random().nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+              child: FlatButton(
+                  onPressed: () {
+                    changeDice();
+                  },
+                  child: Image.asset(
+                    "images/emoji$leftEmoji.jpg",
+                    height: 100,
+                    width: 200,
+                    fit: BoxFit.fitWidth,
+                  ))),
+          Expanded(
+            child: FlatButton(
+                onPressed: () {
+                  changeDice();
+                },
+                child: Image.asset(
+                  "images/emoji$rigthEmoji.jpg",
+                  height: 100,
+                  width: 200,
+                  fit: BoxFit.fitWidth,
+                )),
+          ),
+        ],
+      ),
+    );
+  }
+}
